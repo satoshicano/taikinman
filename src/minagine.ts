989ed098c63a6ebc898d5874517d01c1957effbb
nakamatsu <nakamatsu@fringe81.com>
2018-12-26 13:55:12 +0900

    hotfix

--- src/minagine.ts
+++ src/minagine.ts
@@ -11,13 +11,13 @@ if (!credential.has("email") || !credential.has("password")) {
   process.exit(1);
 }
 
-const email = credential.get("email");
-const password = credential.get("password");
-const companyDomain = email.split("@")[1];
+const EMAIL = credential.get("email");
+const PASSWORD = credential.get("password");
+const COMPANY_DOMAIN = EMAIL.split("@")[1];
 
-type command = "home" | "work";
+const LOGIN_URL = "https://tm.minagine.net/index.html";
 
-export async function doSomethingInMinagine(cmd: command) {
+export async function doSomethingInMinagine(cmd: "home" | "work") {
   const browser = await puppeteer.launch({
     headless: false,
     executablePath: "/usr/bin/chromium-browser"
@@ -28,32 +28,38 @@ export async function doSomethingInMinagine(cmd: command) {
     height: 800
   });
 
-  await page.goto("https://tm.minagine.net/index.html");
+  await page.goto(LOGIN_URL);
 
   await page.waitForSelector("#login_form > div:nth-child(5) > div > input");
-  await page.type("#user_cntrctr_dmn", companyDomain);
-  await page.type("#user_login", email);
-  await page.type("#user_password", password);
+  await page.type("#user_cntrctr_dmn", COMPANY_DOMAIN);
+  await page.type("#user_login", EMAIL);
+  await page.type("#user_password", PASSWORD);
 
   page.click("#login_form > div:nth-child(5) > div > input");
 
   if (cmd === "work") {
     await page.waitForSelector("#button0");
-    page.click("#button0");
+    await page.evaluate(() => {
+      const workButton = document.querySelector("#button0") as HTMLElement;
+      workButton.click();
+    });
   }
 
   if (cmd === "home") {
     await page.waitForSelector("#button1");
-    page.click("#button1");
+    await page.evaluate(() => {
+      const homeButton = document.querySelector("#button1") as HTMLElement;
+      homeButton.click();
+    });
   }
 
-  const clip = await page.evaluate(s => {
-    const el = document.querySelector(s);
+  await page.waitForSelector("table.none_sortable_table");
+  const clip = await page.evaluate(() => {
+    const el = document.querySelector("table.none_sortable_table");
     const { width, height, top: y, left: x } = el.getBoundingClientRect();
     return { width, height, x, y };
-  }, ".none_sortable_table");
+  });
 
-  await page.waitForNavigation({ waitUntil: "load" });
   await page.screenshot({ clip, path: "result.png" });
 
   await browser.close();
