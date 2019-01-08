07731ffd26457f876101495d34b392e29f50d903
Satoshi Nakamatsu <satoshicano@gmail.com>
2019-01-08 12:29:18 +0900

    v0.1 (#7)

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,27 @@ import NfcpyId from "node-nfcpy-id";
 import * as Configstore from "configstore";
 import { doSomethingInMinagine } from "./minagine";
 import { sendResultToSlack, sendAlertToSlack } from "./slack";
+import * as Sentry from "@sentry/node";
 
 const credential = new Configstore("taikinman", {}, { globalConfigPath: true });
 
-if (!credential.has("slackToken") || !credential.has("conversationId")) {
+if (
+  !credential.has("slackToken") ||
+  !credential.has("conversationId") ||
+  !credential.has("sentryDsn")
+) {
   console.error("Need to edit credential file.");
-  console.log(`Set 'uid' in config file located at ${credential.path}`);
+  console.log(
+    `Set 'slackToken', 'conversationId', 'sentryDsn' in config file located at ${
+      credential.path
+    }`
+  );
   process.exit(1);
 }
+
+Sentry.init({
+  dsn: credential.get("sentryDsn")
+});
 
 const nfc = new NfcpyId();
 const uid = credential.get("uid");
@@ -21,11 +34,15 @@ nfc.on("touchstart", card => {
     if (now.getHours() < 15) {
       // start work
       console.log("touchstart", "id:", card.id, "type:", card.type);
-      doSomethingInMinagine("work").then(sendResultToSlack);
+      doSomethingInMinagine("work")
+        .then(sendResultToSlack)
+        .catch(console.error);
     } else {
       // go home
       console.log("touchstart", "id:", card.id, "type:", card.type);
-      doSomethingInMinagine("home").then(sendResultToSlack);
+      doSomethingInMinagine("home")
+        .then(sendResultToSlack)
+        .catch(console.error);
     }
   } else {
     sendAlertToSlack(card.id);
--- src/minagine.ts
+++ src/minagine.ts
@@ -11,15 +11,16 @@ if (!credential.has("email") || !credential.has("password")) {
   process.exit(1);
 }
 
-const email: string = credential.get("email");
-const password = credential.get("password");
-const companyDomain = email.split("@")[1];
+const EMAIL = credential.get("email");
+const PASSWORD = credential.get("password");
+const COMPANY_DOMAIN = EMAIL.split("@")[1];
 
-type command = "home" | "work";
+const LOGIN_URL = "https://tm.minagine.net/index.html";
 
-export async function doSomethingInMinagine(cmd: command) {
+export async function doSomethingInMinagine(cmd: "home" | "work") {
   const browser = await puppeteer.launch({
-    headless: false
+    headless: false,
+    executablePath: "/usr/bin/chromium-browser"
   });
   const page = await browser.newPage();
   page.setViewport({
@@ -27,26 +28,39 @@ export async function doSomethingInMinagine(cmd: command) {
     height: 800
   });
 
-  await page.goto("https://tm.minagine.net/index.html");
+  await page.goto(LOGIN_URL);
 
-  await page.type("#user_cntrctr_dmn", companyDomain);
-  await page.type("#user_login", email);
-  await page.type("#user_password", password);
+  await page.waitForSelector("#login_form > div:nth-child(5) > div > input");
+  await page.type("#user_cntrctr_dmn", COMPANY_DOMAIN);
+  await page.type("#user_login", EMAIL);
+  await page.type("#user_password", PASSWORD);
 
   page.click("#login_form > div:nth-child(5) > div > input");
 
   if (cmd === "work") {
     await page.waitForSelector("#button0");
-    page.click("#button0");
+    await page.evaluate(() => {
+      const workButton = document.querySelector("#button0") as HTMLElement;
+      workButton.click();
+    });
   }
 
   if (cmd === "home") {
     await page.waitForSelector("#button1");
-    page.click("#button1");
+    await page.evaluate(() => {
+      const homeButton = document.querySelector("#button1") as HTMLElement;
+      homeButton.click();
+    });
   }
 
-  await page.waitForNavigation({ waitUntil: "load" });
-  await page.screenshot({ path: "result.png" });
+  await page.waitForSelector("table.none_sortable_table");
+  const clip = await page.evaluate(() => {
+    const el = document.querySelector("table.none_sortable_table");
+    const { width, height, top: y, left: x } = el.getBoundingClientRect();
+    return { width, height, x, y };
+  });
+
+  await page.screenshot({ clip, path: "result.png" });
 
   await browser.close();
 }
